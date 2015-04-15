c57c2ebce03b4de4ed03571417d0796b25502dc3
Omri Marcovitch <omrim@mellanox.com>
2015-04-15 15:35:09 +0300

    Add build package
    The builder will create RPM file for RH7 or RH6
    
    Signed-off-by: Omri Marcovitch <omrim@mellanox.com>

---
+++ build_package/build.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Copyright (c) 2014 Mellanox Technologies. All rights reserved.
+#
+# This Software is licensed under one of the following licenses:
+#
+# 1) under the terms of the "Common Public License 1.0" a copy of which is
+#    available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/cpl.php.
+#
+# 2) under the terms of the "The BSD License" a copy of which is
+#    available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/bsd-license.php.
+#
+# 3) under the terms of the "GNU General Public License (GPL) Version 2" a
+#    copy of which is available from the Open Source Initiative, see
+#    http://www.opensource.org/licenses/gpl-license.php.
+#
+# Licensee has the right to choose one of the above licenses.
+#
+# Redistributions of source code must retain the above copyright
+# notice and one of the license notices.
+#
+# Redistributions in binary form must reproduce both the above copyright
+# notice, one of the license notices in the documentation
+# and/or other materials provided with the distribution.
+readonly SUCCESS=0
+readonly FAILURE=1
+readonly RHEL_DIST="el"
+readonly UBUNTU_DIST="ubuntu"
+readonly REDHAT_FILE="/etc/redhat-release"
+readonly UBUNTU_FIlE="/etc/os-release"
+
+PACKAGE_NAME="Ceilometer Mellanox SR-IOV inspector"
+DIST=""
+CIRROS_VERSION="0.3.2"
+CIRROS_RELEASE="1"
+ROOT_RPMBUILD="/root/rpmbuild/"
+BUILD_DEST="$ROOT_RPMBUILD/BUILD/"
+BUILD_TARGET=$(pwd)
+
+function check_dist() {
+    if [  -f ${REDHAT_FILE} ]; then
+        if  grep -q -i "release 6" ${REDHAT_FILE} || grep -q -i "release 7" ${REDHAT_FILE}  ; then
+            DIST=${RHEL_DIST}
+        else
+            echo "$PACKAGE_NAME Support only CentOS and RedHat Release 6 or 7"
+            exit ${FAILURE}
+        fi
+    elif  [  -f ${UBUNTU_FIlE} ]; then
+        DIST=${UBUNTU_DIST}
+    else
+        echo "$PACKAGE_NAME Support only CentOS 6 and Ubuntu"
+        exit ${FAILURE}
+    fi
+}
+
+
+function build_rpm(){
+    \cp -rf ../.git ../mlnx_libvirt ../README.rst ../setup.py ../setup.cfg $BUILD_DEST
+    cp -rf ../README.rst ../setup.py ../setup.cfg $BUILD_DEST
+    rpmbuild -ba centos/mlnx_sriov_ceilometer.spec
+    cp $ROOT_RPMBUILD/RPMS/x86_64/* $BUILD_TARGET
+    cp $ROOT_RPMBUILD/SRPMS/* $BUILD_TARGET
+
+}
+
+function update_deb_version(){
+    sed "s/@@VERSION@@/${CIRROS_VERSION}/g;s/@@RELEASE@@/${CIRROS_RELEASE}/g" debian/changelog.template > debian/changelog
+}
+
+function build_deb(){
+    cp $IMG_FILE ubuntu
+    cd ubuntu
+    update_deb_version
+    dpkg-buildpackage -tc -uc
+}
+
+check_dist
+if [ $DIST == $RHEL_DIST ]; then
+    build_rpm
+elif [ $DIST == $UBUNTU_DIST ]; then
+    build_deb
+fi
+
+echo "Build finished successfully. Build Files are located at: $BUILD_TARGET"
+
+exit ${SUCCESS}
---
+++ build_package/centos/mlnx_sriov_ceilometer.spec
@@ -0,0 +1,39 @@
+Name:       mlnx-sriov-ceilometer
+Version:    2015.1.0
+Release:    1%{?dist}
+Summary:    Mellnaox SR-IOV ceilometer counters inspector
+License:    GPLv2
+BuildRoot:  %(mktemp -ud %{_tmppath}/%{name}-%{version}-%{release}-XXXXXX)
+
+BuildRequires:  python-pbr
+
+Requires:  openstack-ceilometer-compute
+
+%description
+Mellnaox SR-IOV ceilometer counters inspector
+
+%build
+%{__python} setup.py build
+
+%install
+%{__python} setup.py install -O1 --skip-build --root %{buildroot} --install-lib %{python_sitelib}/ceilometer/compute/virt
+
+%files
+%doc README.rst
+%{python_sitelib}/ceilometer/compute/virt
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%post
+ep_file_path=$(find /usr/ -name entry_points.txt | grep  "/ceilometer[^/]*egg-info")
+ep_value="mlnx_libvirt = ceilometer.compute.virt.mlnx_libvirt.inspector:MlnxLibvirtInspector"
+section=ceilometer.compute.virt
+if grep -q "^\[$section\]" $ep_file_path ; then
+	if ! grep --no-message -q "$ep_value" $ep_file_path ; then	
+		sed -i "/\[$section\]/a $ep_value" $ep_file_path
+	fi
+else
+	echo "ERROR: Can not find section: $section in file: $ep_file_path"
+	exit 1
+fi
