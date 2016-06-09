d27899949e9c83831f2e46936ed3ee5ef1ccd34d
Lenny Verkhovsky <lennyb@mellanox.com>
2016-06-09 14:09:46 +0300

    Added tox and fixed pep8

--- mlnx_libvirt/inspector.py
+++ mlnx_libvirt/inspector.py
@@ -16,16 +16,16 @@
 # limitations under the License.
 """Implementation of Inspector abstraction for mlnx_libvirt."""
 
-from lxml import etree
 import os
 import re
 
+from ceilometer import utils
+from lxml import etree
 from oslo_config import cfg
+from oslo_log import log
 
 from ceilometer.compute.virt import inspector as virt_inspector
 from ceilometer.compute.virt.libvirt.inspector import LibvirtInspector
-from ceilometer import utils
-from oslo_log import log
 
 PORT_DIRECT_TYPE = "hostdev"
 COUNTERS_DEST = "/sys/class/net/%(if_name)s/vf%(vf)s/statistics/%(cnt_name)s"
@@ -116,8 +116,8 @@ class MlnxLibvirtInspector(LibvirtInspector):
 
             self._init_vf_counters(name, vf_num)
             stats = virt_inspector.InterfaceStats(
-                                    rx_bytes=self.counters_dic["rx_bytes"],
-                                    rx_packets=self.counters_dic["rx_packets"],
-                                    tx_bytes=self.counters_dic["tx_bytes"],
-                                    tx_packets=self.counters_dic["tx_packets"])
+                rx_bytes=self.counters_dic["rx_bytes"],
+                rx_packets=self.counters_dic["rx_packets"],
+                tx_bytes=self.counters_dic["tx_bytes"],
+                tx_packets=self.counters_dic["tx_packets"])
             yield (interface, stats)
